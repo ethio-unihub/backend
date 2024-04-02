38bb65724064d8197531e654e077db5e9f38a9bd
tsitiii <ziontaa9@gmail.com>
2024-04-02 12:37:43 +0300

    Core app task completed

--- core/models.py
+++ core/models.py
@@ -3,4 +3,14 @@ from django.conf import settings
 
 user_model = settings.AUTH_USER_MODEL
 
+class Organization(models.Model):
+    name= models.CharField(unique=True, max_length=200)
+    logo=models.ImageField(upload_to="")
+    address=models.CharField(max_length=150)
 
+
+class Hashtag(models.Model):
+    name=models.CharField(max_length=200)
+    organization=models.ForeignKey(Organization, related_name="hashtags",on_delete=models.CASCADE)
+    subscribers=models.PositiveBigIntegerField(null=True)
+    slug=models.SlugField(unique=True)
--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from .models import  Hashtag
+from .serializers import *
+
 
 @api_view()
 def documentation(request):
@@ -9,3 +12,12 @@ def documentation(request):
         "register user": "auth/users/"
     }
     return Response(routes)
+
+class HashtagsApi(viewsets.ModelViewSet):
+    queryset = Hashtag.objects.all()
+    serializer_class = HashtagSerializer
+    http_method_names = ['get']
+
+    def list(self, request):
+        serializer = self.serializer_class(self.queryset, many=True)
+        return Response(serializer.data)
